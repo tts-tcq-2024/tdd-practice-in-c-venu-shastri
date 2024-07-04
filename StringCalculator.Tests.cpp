0c30e5471cff52e5e673b7fe37eadae269e96905
Venugopal K <venu.shastri@gmail.com>
2024-07-04 16:48:39 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -7,14 +7,14 @@ TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
     const char* input = "0";
     int result = add(input);
     ASSERT_EQ(result, expectedresult);
 }
-
+/*
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
     const char*  input = "1,2";
