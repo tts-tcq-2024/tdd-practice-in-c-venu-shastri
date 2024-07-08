d2a68bf88babf499943b9cdcc008f183e53ad889
Venugopal K <venu.shastri@gmail.com>
2024-07-08 12:34:43 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,14 +1,4 @@
 
-int isEmptyString(const char *str) {
-    // Check if the first character is the null terminator
-    if (str == NULL || str[0] == '\0') {
-        return 1; // The string is empty
-    }
-    return 0; // The string is not empty
-}
 int add(const char* input){
-  if (isEmptyString(input)) {
-        return 0; // The string is empty
-    }
-return -1;
+ return NULL;
 }
