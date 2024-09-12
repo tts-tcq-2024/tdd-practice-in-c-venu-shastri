537e48defa41daa6accb3bf8e9f066c978e69556
Venugopal K <venu.shastri@gmail.com>
2024-09-12 17:03:42 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,11 +1,1 @@
-bool isEmptyString(const char*  input){
-if((input == NULL || input[0] == '\0')){
-          return 1;
-}
-return 0;
-}
 
-int add(const char* input){
-     if( isEmptyString(input)) { return 0;}
-     return -1;
-}
